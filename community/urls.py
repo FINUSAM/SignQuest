efb4138061b76cb5f9a1f04578a6cfbf64f7fa2d
FINUSAM <faheemsameer10@gmail.com>
2024-04-29 18:32:02 +0530

    community page backend complete

--- community/urls.py
+++ community/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.default_community),
     path('<group>/', views.community, name='community'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import GroupModel, MessageModel
 
 # Create your views here.
 
 @login_required
+def default_community(request):
+    return redirect('community', 'universe')
+
+@login_required
 def community(request, group='main'):
-    return render(request, 'community/community.html')
+    if request.method == 'POST':
+        if group=='create_group':
+            new_group_name = request.POST.get('new_group_name')
+            if new_group_name:
+                GroupModel.objects.create(group_name=new_group_name)
+            else:
+                print('xxxxxxxxxxxxxxxxxxxxxxx')
+        elif group != 'main':
+            message = request.POST.get('message')
+            group_id = GroupModel.objects.get(group_name=group)
+            MessageModel.objects.create(message=message, author=request.user, group=group_id)
+        return redirect('community', group=group)
+    else:
+        groups = GroupModel.objects.all()
+        if group == 'main' or group=='create_group':
+            community_messages = None
+        else:
+            group_id = GroupModel.objects.get(group_name=group).id
+            community_messages = MessageModel.objects.filter(group=group_id)
+        return render(request, 'community/community.html', {'groups': groups, 'current_group': group, 'messages': community_messages}) 
