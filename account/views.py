0c0f544cdd4c1c45f13f12c0256bf5a3ccb5bbfd
FINUSAM <faheemsameer10@gmail.com>
2024-05-07 13:10:22 +0530

    accounts page completed

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+import json
 from django.apps import apps
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
@@ -6,13 +7,27 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .models import UserStatsModel
+import matplotlib.pyplot as plt
+from io import BytesIO
 
 # Create your views here.
 @login_required
 def account(request):
     user_stats = UserStatsModel.objects.get(user=request.user)
+    field_names = [field.name for field in UserStatsModel._meta.get_fields()]
+    values = [getattr(user_stats, field) for field in field_names]
+    
+    alphabet_confidence_chart = {
+        'labels': list(field_names[3:38]),
+        'data': list([(1 - value)*100 if value != 0 else 0 for value in values[3:38]])
+    }
 
-    data = {'user_stats' : user_stats}
+    data = {
+        'alphabet_confidence_chart': json.dumps(alphabet_confidence_chart),
+        'user_stats': user_stats
+    }
+
+
     return render(request, "account/account.html", data)
 
 def login_view(request):
